9b2e3718439003e346af780502511332d0a64e71
Muzammil.TheDeveloper <muzans786@gmail.com>
2025-07-22 08:41:29 -0700

    Update HeroSections.jsx

--- src/components/UI/HeroSections.jsx
+++ src/components/UI/HeroSections.jsx
@@ -18,7 +18,7 @@ export const HeroSection = () => {
         </div>
         <div className="hero-image">
           <img
-            src="src\assets\image.jpeg"
+            src="src\assets\image.jpeg.jpeg"
             alt="world is beauty"
             className="banner-image"
             
