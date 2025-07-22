9c162527d3537320e2b3fe5e6a37232204c5069b
Muzammil.TheDeveloper <muzans786@gmail.com>
2025-07-22 09:47:32 -0700

    Update HeroSections.jsx

--- src/components/UI/HeroSections.jsx
+++ src/components/UI/HeroSections.jsx
@@ -1,5 +1,5 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
-import heroImage from "../assets/image.jpeg"; 
+import heroImage from "../../assets/image.jpeg";
 
 export const HeroSection = () => {
   return (
